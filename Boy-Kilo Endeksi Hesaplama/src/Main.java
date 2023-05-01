6b93493011079db06c408cc193da8bea0587028b
melihtezel <melihtezel2001@gmail.com>
2023-05-01 20:39:37 +0300

    Java Examples

---
+++ Boy-Kilo Endeksi Hesaplama/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Boyunuzu girin:");
+		double boy=scan.nextDouble();
+		System.out.println("Kilonuzu girin:");
+		double kilo=scan.nextDouble();
+		
+		double endeks=kilo/(boy*boy);
+		System.out.println("endeksiniz: " +endeks);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
